175b76397509187c556ff6bd32b790d2fb62e045
Skiexx <codeskiexx@yandex.ru>
2024-01-11 13:43:01 +0700

    edit to curl

--- src/Esia/Signer/HttpSigner.php
+++ src/Esia/Signer/HttpSigner.php
@@ -8,14 +8,6 @@ class HttpSigner
     extends AbstractSignerPKCS7
     implements SignerInterface
 {
-
-    /**
-     * @var string
-     */
-    private string $signingServerUrl;
-    private string $headers = '';
-    private string $method;
-
     /**
      * @param string $signingServerUrl
      * @param array $headers
@@ -23,16 +15,10 @@ class HttpSigner
      * @noinspection PhpMissingParentConstructorInspection
      */
     public function __construct(
-        string $signingServerUrl,
-        array $headers,
-        string $method,
+        private string $signingServerUrl,
+        private array $headers,
+        private string $method,
     ) {
-        $this->signingServerUrl = $signingServerUrl;
-        $this->method = $method;
-
-        foreach ($headers as $key => $val) {
-            $this->headers .= "$key: $val" . PHP_EOL;
-        }
     }
 
     /**
@@ -45,30 +31,25 @@ class HttpSigner
         $data = json_encode(['text' => $message], JSON_THROW_ON_ERROR);
 
         $options = [
-            'http' => [
-                'header' => $this->headers,
-                'method' => $this->method,
-                'content' => $data,
-            ],
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER         => false,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_USERAGENT      => "spider",
+            CURLOPT_AUTOREFERER    => true,
+            CURLOPT_CONNECTTIMEOUT => 120,
+            CURLOPT_TIMEOUT        => 120,
+            CURLOPT_CUSTOMREQUEST  => $this->method,
+            CURLOPT_POSTFIELDS     => $data,
+            CURLOPT_HTTPHEADER     => $this->headers
         ];
 
-        $context = stream_context_create($options);
-        $result = file_get_contents(
-            $this->signingServerUrl,
-            false,
-            $context
-        );
+        $ch = curl_init($this->signingServerUrl);
+        curl_setopt_array($ch, $options);
+        $content = curl_exec($ch);
+        curl_close($ch);
 
-        $content = json_decode(
-            $result,
-            true,
-            512,
-            JSON_THROW_ON_ERROR
-        );
-
-        /** @noinspection PhpUnnecessaryLocalVariableInspection */
-        $sign = $content['result'] ?? '';
-
-        return $sign;
+        return $content;
     }
 }
